e43599c99e52aab12b1caf11bdd11541c2ec8765
PragyaBhootra <Pbhootra2005@gmail.com>
2025-01-18 17:59:35 +0530

    Update selection_sort.js

--- sorting visualizer/Sorting_Visualizer/scripts/selection_sort.js
+++ sorting visualizer/Sorting_Visualizer/scripts/selection_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   PRAGYA BHOOTRA
     
     *****************
 */
@@ -61,7 +61,7 @@ function Selection_sort()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   PRAGYA BHOOTRA
     
     *****************
 */
